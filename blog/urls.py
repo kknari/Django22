4a0aaee842dbe0c1834cde84d8c4146207b8647f
kknari <nari907@duksung.ac.kr>
2022-09-29 13:09:47 +0900

    CBV로 포스트 목록/상세 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [ #IP주소/blog/
-    path('', views.index), #IP/blog/ 블로그 앱 밑에 있는 views로 가겠다
-    path('<int:pk>/', views.single_post_page)
+    path('', views.PostList.as_view()),
+    path('<int:pk>/', views.PostDetail.as_view())
+    # path('', views.index), #IP/blog/ 블로그 앱 밑에 있는 views로 가겠다
+    # path('<int:pk>/', views.single_post_page)
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
 
-def index(request):
-    posts = Post.objects.all().order_by('-pk') # 정렬해 주는 명렁어 'pk'는 1 2 3으로 정렬
-    return render(request, 'blog/index.html', {'posts': posts})
+class PostList(ListView):
+    model = Post
+    ordering = '-pk' #데이터 많이 들어가서 정렬 필요 밑에 상세페이지는 정렬 필요 x
+    #템플릿은 모델명_list.html: post_list.html
 
-def single_post_page(request, pk):
-    post = Post.objects.get(pk=pk) #오른쪽은 전달받은 pk
-    return render(request, 'blog/single_post_page.html', {'post': post})
-    #오른쪽에 있는 게 내가 선언해 준 거, 왼쪽은 템플릿에서 전달받는 인수 값
+class PostDetail(DetailView):
+    model = Post
+    # 템플릿은 모델명_detail.html: post_detail.html
+
+
+# def index(request):
+#     posts = Post.objects.all().order_by('-pk') # 정렬해 주는 명렁어 'pk'는 1 2 3으로 정렬
+#     return render(request, 'blog/index.html', {'posts': posts})
+#
+# def single_post_page(request, pk):
+#     post = Post.objects.get(pk=pk) #오른쪽은 전달받은 pk
+#     return render(request, 'blog/single_post_page.html', {'post': post})
+#     #오른쪽에 있는 게 내가 선언해 준 거, 왼쪽은 템플릿에서 전달받는 인수 값
