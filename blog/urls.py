9df1507846dafc7c76134f1c39f6ab716c5db620
kknari <nari907@duksung.ac.kr>
2022-11-12 23:30:42 +0900

    포스트 작성 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [ #IP주소/blog/
     path('<int:pk>/', views.PostDetail.as_view()),
     path('category/<str:slug>/', views.category_page), # IP주소/blog/category/slug/
     path('tag/<str:slug>/', views.tag_page), # IP주소/blog/tag/slug/
+    path('create_post/', views.PostCreate.as_view()),
+
     # path('', views.index), #IP/blog/ 블로그 앱 밑에 있는 views로 가겠다
     # path('<int:pk>/', views.single_post_page)
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,34 @@
 from django.shortcuts import render
 from .models import Post, Category, Tag
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.shortcuts import render, redirect
 
 # Create your views here.
 #view에서 사용자 요구 처리
+
+class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
+    model = Post
+    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category']
+    # 모델명_form.html
+
+    def test_func(self):
+        return self.request.user.is_superuser or self.request.user.is_staff
+
+    def form_valid(self, form):
+        current_user = self.request.user
+        if current_user.is_authenticated and (current_user.is_superuser or self.request.user.is_staff):
+            form.instance.author = current_user
+            return super(PostCreate, self).form_valid(form)
+        else:
+            return redirect('/blog/')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(PostCreate, self).get_context_data()
+        context['categories'] = Category.objects.all()  # 따옴표 안에 있는 걸 변수로, view로 전달한다
+        context['no_category_post_count'] = Post.objects.filter(category=None).count()
+        return context
+
 
 class PostList(ListView):
     model = Post
