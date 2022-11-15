945bc31357d6362feb31a9e7afe2a767b508be10
kknari <nari907@duksung.ac.kr>
2022-11-15 15:07:44 +0900

    태그 선택란 추가하기

--- blog/views.py
+++ blog/views.py
@@ -4,13 +4,14 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.shortcuts import render, redirect
 from django.core.exceptions import PermissionDenied
+from django.utils.text import slugify
 
 # Create your views here.
 #view에서 사용자 요구 처리
 
 class PostUpdate(LoginRequiredMixin, UpdateView):
     model = Post
-    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category', 'tags']
+    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category']
     template_name = 'blog/post_update_form.html'
 
     def dispatch(self, request, *args, **kwargs):
@@ -18,8 +19,32 @@ class PostUpdate(LoginRequiredMixin, UpdateView):
             return super(PostUpdate, self).dispatch(request, *args, **kwargs)
         else:
             raise PermissionDenied
+
+    def form_valid(self, form):
+        response = super(PostUpdate, self).form_valid(form)
+        self.object.tags.clear()
+
+        tags_str = self.request.POST.get('tags_str')
+        if tags_str:
+            tags_str = tags_str.strip()
+            tags_str = tags_str = tags_str.replace(',', ';')  # ,도 ;로 바꿔 주도록
+            tags_list = tags_str.split(';')  # 단어 단위로 배열 형태로 저장
+            for t in tags_list:
+                t = t.strip()
+                tag, is_tag_created = Tag.objects.get_or_create(name=t)
+                if is_tag_created:
+                    tag.slug = slugify(t, allow_unicode=True)
+                    tag.save()
+                self.object.tags.add(tag)
+
+        return response
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(PostUpdate, self).get_context_data()
+        if self.object.tags.exists():
+            tags_str_list = list()
+            for t in self.object.tags.all():
+                tags_str_list.append(t.name)
+            context['tags_str_default'] = ';'.join(tags_str_list)
         context['categories'] = Category.objects.all()  # 따옴표 안에 있는 걸 변수로, view로 전달한다
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
         return context
@@ -33,10 +58,26 @@ class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
         return self.request.user.is_superuser or self.request.user.is_staff
 
     def form_valid(self, form):
+        # 폼 열 때 이 폼이 유효한가? 유저 체크
         current_user = self.request.user
         if current_user.is_authenticated and (current_user.is_superuser or self.request.user.is_staff):
             form.instance.author = current_user
-            return super(PostCreate, self).form_valid(form)
+            response = super(PostCreate, self).form_valid(form)
+
+            tags_str = self.request.POST.get('tags_str')
+            if tags_str:
+                tags_str = tags_str.strip()
+                tags_str = tags_str = tags_str.replace(',', ';')  # ,도 ;로 바꿔 주도록
+                tags_list = tags_str.split(';')  # 단어 단위로 배열 형태로 저장
+                for t in tags_list:
+                    t = t.strip()
+                    tag, is_tag_created = Tag.objects.get_or_create(name=t)
+                    if is_tag_created:
+                        tag.slug = slugify(t, allow_unicode=True)
+                        tag.save()
+                    self.object.tags.add(tag)
+
+            return response
         else:
             return redirect('/blog/')
 
